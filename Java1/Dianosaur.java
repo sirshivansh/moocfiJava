cc2a598fdc3823b39fc24c6363b7855ce504c85c
Shivansh Mishra <shivnsh01@gmail.com>
2026-03-15 22:14:39 +0530

    Once upon a time, there was a Dinosaur

---
+++ Java1/Dianosaur.java
@@ -0,0 +1,7 @@
+public class Dianosaur {
+    public static void main(String args[]){
+        System.out.println("Once upon a time");
+        System.out.println("there was a");
+        System.out.println("Dinosaur");
+    }
+}
