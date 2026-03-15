1a4209a1a6f9e5379ec4f657769fd46380121963
Shivansh Mishra <shivnsh01@gmail.com>
2026-03-15 23:51:37 +0530

    Conversation using Scanner class

---
+++ Java1/Conversation.java
@@ -0,0 +1,26 @@
+/*
+Write a program that works as follows:
+
+Sample output
+Greetings! How are you doing?
+Good thank you!
+Oh, how interesting. Tell me more!
+Well, there's really nothing to tell.
+Thanks for sharing!
+*/
+
+import java.util.Scanner;
+
+public class Conversation
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Greetings! How are you doing?");
+        String a = sc.nextLine();
+        System.out.println("Oh, how interesting. Tell me more!");
+        String b = sc.nextLine();
+        System.out.println("Thanks for sharing!");
+    }
+}
