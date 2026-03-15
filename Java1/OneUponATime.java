8ba4bdc2272a5db5af52c14162449621d3e3270b
Shivansh Mishra <shivnsh01@gmail.com>
2026-03-15 22:12:20 +0530

    Once Upon a time, there was a program

---
+++ Java1/OneUponATime.java
@@ -0,0 +1,7 @@
+public class OneUponATime {
+    public static void main(String args[]){
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a program");
+    }
+}
