0fb8d64d91aa91af28498557bbdc0f7c2506262c
Shivansh Mishra <shivnsh01@gmail.com>
2026-03-15 22:31:18 +0530

    Taking user input and printing it

---
+++ Java1/Message.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Message {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write a message:");
+        String message = sc.nextLine();
+        System.out.println(message);
+    }
+}
