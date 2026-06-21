72756490a41526a4a9537538f595ae81af325a02
Shivansh Mishra <shivnsh01@gmail.com>
2026-06-21 16:55:19 +0530

    Checking the Age

---
+++ Java1/CheckingTheAge.java
@@ -0,0 +1,18 @@
+// Write a program that prompts the user to input their age and checks whether or not it is possible 
+// (at least 0 and at most 120). Only use a single ifcommand in your program.
+
+import java.util.Scanner;
+
+public class CheckingTheAge{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How old are you? ");
+        int x = Integer.valueOf(sc.nextLine());
+
+        if(x>=0 && x<=120){
+            System.out.println("OK");
+        }else{
+            System.out.println("Impossible!");
+        }
+    }
+}
